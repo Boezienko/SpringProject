7cc22d426fd06fbe5c5637a1138c803b99f1c7d0
Boezienko <boeazienko@gmail.com>
2024-07-30 16:27:30 -0500

    Made starter spring boot project
    Made starter spring boot project using https://start.spring.io
    With these properties:
    Project: Gradle-Kotlin
    Language-Java
    Spring Boot-3.3.2
    Packaging-Jar
    Java-22
    Dependencies:
     - SpringBootDevTools
     - Thymeleaf
     - Spring Web
     - Spring Data JPA
     - H2 Database
     - Validation
     - Lombok

---
+++ SharingApp-Spring/SharingApp-Spring/src/main/java/com/example/SharingApp_Spring/SharingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SharingApp_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingAppSpringApplication.class, args);
+	}
+
+}
---
+++ SharingApp-Spring/SharingApp-Spring/src/test/java/com/example/SharingApp_Spring/SharingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SharingApp_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
